ff4949fbb42f9a50d17e0e1b75cf4b21ad8e1378
moatasem mamdouh <moatasem.mamdouh66@gmail.com>
2024-12-31 12:42:49 +0200

    Add project files.

---
+++ TestConflict/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TestConflict
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("test conflict from branch one ");
+
+        }
+    }
+}
